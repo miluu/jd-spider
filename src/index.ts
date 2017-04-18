f4c422ca702bf7ba910f1e8cef679d187fa492e6
miluu <huangyx@qq.com>
2017-04-18 11:22:16 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
-import Test from './Test';
+import * as superagent from 'superagent';
+import * as cheerio from 'cheerio';
+import * as _ from 'lodash';
 
-const t = new Test('J8');
-t.say();
+console.log('Start...');
+
+superagent.get('https://cnodejs.org/')
+  .end(function (err, res) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log('Finish:');
+    const $ = cheerio.load(res.text);
+    const items: any[] = [];
+    $('#topic_list .topic_title').each ((idx, element) => {
+      const $element = $(element);
+      items.push({
+        title: $element.attr('title'),
+        href: $element.attr('href')
+      });
+    });
+    const output = _.map(items, (item, index) => `${index + 1}. ${item.title}`);
+    console.log(output.join('\r\n'));
+  });
