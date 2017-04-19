67b691a23ebc575fe8ab1c5da9dcd4967744fc5e
miluu <hyxiong@qq.com>
2017-04-19 22:41:46 +0800

    add log4js

--- src/Main.ts
+++ src/Main.ts
@@ -1,10 +1,43 @@
 import * as http from 'http';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as Promise from 'bluebird';
+import * as log4js from 'log4js';
 
+log4js.configure({
+  appenders: [
+    {type: 'console'},
+    {type: 'file', filename: path.join(__dirname, '../log/jd-spider.log'), category: 'spider'}
+  ]
+});
+const log = log4js.getLogger('spider');
+
+log.info('App start...')
 export default class Main {
   constructor() {
-    this._readConfig();
+    // this._readConfig();
+    const p1 = new Promise((resolve, reject) => {
+      setTimeout(function() {
+        resolve('hello');
+      }, 1000);
+    });
+    const p2 = new Promise((resolve, reject) => {
+      const b = Math.random() > 0.5;
+      setTimeout(function() {
+        b ? resolve('world'): reject('What the fxxk!');
+      }, 2000);
+    });
+    p1
+      .then((data) => {
+        log.debug(data.toString());
+        return p2;
+      })
+      .then((data) => {
+        log.debug(data.toString());
+      })
+      .catch((err) => {
+        log.error(err.toString());
+      });
   }
 
   private _readConfig () {
