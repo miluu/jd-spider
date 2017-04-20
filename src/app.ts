d5a1f39788d113040e3df8fa9a5942b6be93a8c7
miluu <huangyx@qq.com>
2017-04-20 18:17:10 +0800

    create thumb

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
+import * as gm from 'gm';
+import * as fs from 'fs';
+import * as path from 'path';
 import logger from './logger';
 import item from './item';
+import {
+  FILES_PATH
+} from './paths';
 
 module app {
   export function run () {
     logger.info('App start.');
-    item.getItem('https://item.m.jd.com/product/11781267.html');
+    // item.getItem('https://item.m.jd.com/product/11781267.html');
+    gm(path.join(FILES_PATH, 'img/xxx/test.jpg'))
+      .resize(null, 320)
+      .write(path.join(FILES_PATH, 'img/xxx/test_thumb.jpg'), (err) => {
+        if (err) {
+          logger.error(err.toString());
+          return;
+        }
+        logger.info('Create thumb success.');
+      });
   }
 }
 
