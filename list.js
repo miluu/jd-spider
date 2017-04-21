6320a6b86fbc26bd887ba3639c5c5271c7f39981
miluu <huangyx@qq.com>
2017-04-21 17:28:57 +0800

    catelogy

---
+++ list.js
@@ -0,0 +1,5 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+const catelogy = require('./built/catelogy').default;
+
+catelogy.getList();
--- src/catelogy.ts
+++ src/catelogy.ts
@@ -1,5 +1,35 @@
-module catelogy {
+import * as path from 'path';
+import * as _ from 'lodash';
+import util from './util';
+import {CONFIG_PATH} from './paths';
+import logger from './logger';
 
+module catelogy {
+  interface ICatelogy {
+    name: string;
+    path: string;
+    [key: string]: any;
+  }
+  interface IBigCatelogy {
+    name: string;
+    catelogyList: ICatelogy[];
+    [key: string]: any;
+  }
+
+  const catelogyList: ICatelogy[] = getCatelogyList();
+
+  export function getList() {
+    logger.debug('', catelogyList);
+  }
+
+  function getCatelogyList () {
+    const bigCatelogyList: IBigCatelogy[] = util.readJson(path.join(CONFIG_PATH, 'catelogy-list.json'));
+    let _catelogyList: ICatelogy[] = [];
+    _.forEach(bigCatelogyList, (bigCatelogy) => {
+      _catelogyList.concat(bigCatelogy.catelogyList);
+    });
+    return _catelogyList;
+  }
 }
 
 export default catelogy;
